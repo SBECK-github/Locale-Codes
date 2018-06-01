681107f4804890613f11c75b87b76519a451cce5
Sullivan Beck <sbeck@cpan.org>
2018-06-01 09:24:27 -0400

    Initial checkin of next release cycle: 3.58

--- lib/Locale/Codes/Constants.pm
+++ lib/Locale/Codes/Constants.pm
@@ -15,7 +15,7 @@ require 5.006;
 use Exporter qw(import);
 
 our($VERSION,@EXPORT);
-$VERSION   = '3.57';
+$VERSION   = '3.58';
 
 ################################################################################
 our(@CONSTANTS,%ALL_CODESETS);
--- lib/Locale/Codes/LangExt_Retired.pm
+++ lib/Locale/Codes/LangExt_Retired.pm
@@ -11,7 +11,7 @@ use warnings;
 use utf8;
 
 our($VERSION);
-$VERSION='3.57';
+$VERSION='3.58';
 
 $Locale::Codes::Retired{'langext'}{'alpha'}{'code'} = {
    q(rsi) => q(Rennellese Sign Language),
