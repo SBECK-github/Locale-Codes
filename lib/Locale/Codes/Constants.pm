d70bbfc26d681ab8fab1a5c3ca6a0c95dd810f9a
Sullivan Beck <sbeck@cpan.org>
2018-12-03 12:39:35 -0500

    Initial checkin of next release cycle: 3.60

--- lib/Locale/Codes/Constants.pm
+++ lib/Locale/Codes/Constants.pm
@@ -15,7 +15,7 @@ require 5.006;
 use Exporter qw(import);
 
 our($VERSION,@EXPORT);
-$VERSION   = '3.59';
+$VERSION   = '3.60';
 
 ################################################################################
 our(@CONSTANTS,%ALL_CODESETS);
--- lib/Locale/Codes/LangVar_Retired.pm
+++ lib/Locale/Codes/LangVar_Retired.pm
@@ -11,7 +11,7 @@ use warnings;
 use utf8;
 
 our($VERSION);
-$VERSION='3.59';
+$VERSION='3.60';
 
 $Locale::Codes::Retired{'langvar'}{'alpha'}{'code'} = {
    q(arevela) => q(Eastern Armenian),
