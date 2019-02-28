ed1a97e22d110121ef9b93f27225702c1d2805cf
Sullivan Beck <sbeck@cpan.org>
2019-02-28 12:17:43 -0500

    Initial checkin of next release cycle: 3.61

--- lib/Locale/Codes/Constants.pm
+++ lib/Locale/Codes/Constants.pm
@@ -15,7 +15,7 @@ require 5.006;
 use Exporter qw(import);
 
 our($VERSION,@EXPORT);
-$VERSION   = '3.60';
+$VERSION   = '3.61';
 
 ################################################################################
 our(@CONSTANTS,%ALL_CODESETS);
--- lib/Locale/Codes/Country_Retired.pm
+++ lib/Locale/Codes/Country_Retired.pm
@@ -11,7 +11,7 @@ use warnings;
 use utf8;
 
 our($VERSION);
-$VERSION='3.60';
+$VERSION='3.61';
 
 $Locale::Codes::Retired{'country'}{'alpha-2'}{'code'} = {
    q(an) => q(Netherlands Antilles),
