1f454a32240b4cfcb1cfdb367a7588097467620b
Sullivan Beck <sbeck@cpan.org>
2023-03-06 13:28:11 -0500

    Initial checkin of next release cycle: 3.74

--- lib/Locale/Codes/Constants.pm
+++ lib/Locale/Codes/Constants.pm
@@ -15,7 +15,7 @@ require 5.006;
 use Exporter qw(import);
 
 our($VERSION,@EXPORT);
-$VERSION   = '3.73';
+$VERSION='3.74';
 
 ################################################################################
 our(@CONSTANTS,%ALL_CODESETS);
--- lib/Locale/Codes/Script_Retired.pm
+++ lib/Locale/Codes/Script_Retired.pm
@@ -11,7 +11,7 @@ use warnings;
 use utf8;
 
 our($VERSION);
-$VERSION='3.73';
+$VERSION='3.74';
 
 $Locale::Codes::Retired{'script'}{'alpha'}{'code'} = {
 };
